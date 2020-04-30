1705c4d2fb9c4c4ab8102cd7531c2682bbcccfe6
davidimm70 <62717619+davidimm70@users.noreply.github.com>
2020-04-30 17:16:16 -0400

    Add files via upload

---
+++ MMPProject_2020_04_30_21_12_53/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+createCanvas(200,200)
+}
+
+function draw() {
+	 stroke(10,200);
+  fill(255,87,0);
+  triangle(200,190,20,10,10,200);
+  stroke(309,600);
+  fill(255,189,51);
+  ellipse(50,100,80,80);
+  
+  
+  
+
+}
